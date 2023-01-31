b5edda92a7adc208f7f4dfdf08681bbafc451c52
onxzy <onxzycraft@gmail.com>
2023-01-31 01:10:16 +0100

    more animations

--- index.js
+++ index.js
@@ -1,58 +1,73 @@
 drawMovingText("ONXZY");
 function drawMovingText(text) {
-  const textPadding = [5, -5];
+  const textPadding = -5;
   const baseSpeed = 0.5;
-  const baseFontSize = 100;
+  const fontSize = 110;
   const baseFontWidth = 0.5;
-  const lineNumber = 25;
   const bgColor = '#011627';
-  const txtColor = 'rgba(3, 70, 124, 0.8)';
-  // const txtAlpha = 1;
+  const txtColor = '#033863';
+
+
+
+
+  const canvas = document.getElementById("moving_text_cvs");
+  const context = canvas.getContext("2d"); 
+  const lineNumber = Math.ceil(canvas.height / fontSize)*2;
 
   const speedMultiplier = [];
   const fontSizeMultiplier = [];
-
   for (let i = 0; i < lineNumber; i++) {
     speedMultiplier.push(0.5+Math.random())
     fontSizeMultiplier.push(0.6+0.8*Math.random())
   }
 
-  const canvas = document.getElementById("moving_text_cvs");
-  const context = canvas.getContext("2d");    
-  context.textBaseline = 'top';
-  context.lineWidth = baseFontWidth;
-
-  setInterval(() => {
-    animate()
-  }, 16);
-
-
   const xOffsets = Array(lineNumber)
 
-  function animate() {            
-    // Clear screen
-    context.fillStyle = bgColor;
-    context.globalAlpha = 1;
-    context.fillRect(0, 0, canvas.width, canvas.height);  
-    context.strokeStyle = txtColor;
-    // context.globalAlpha = txtAlpha;
-    
-    var yOffset = 0;
-    for (let i = 0; i < lineNumber; i++) {
-      const textHeight = baseFontSize*fontSizeMultiplier[i%fontSizeMultiplier.length];
-      context.font = `${textHeight}px Inter`;
-      // context.lineWidth = baseFontWidth*fontSizeMultiplier[i%fontSizeMultiplier.length];
+  const mousePositon = [canvas.width/2, canvas.height/2];
+  document.addEventListener('mousemove', (e) => {
+    const rect = canvas.getBoundingClientRect();
+    mousePositon[0] = e.clientX - rect.left;
+    mousePositon[1] = e.clientY - rect.top;
+  });
+  
+  context.fillStyle = bgColor;
+  context.textBaseline = 'top';
+  context.lineWidth = baseFontWidth;  
+  
+  setInterval(() => {
+    animate()
+  }, 10);
 
-      drawLine(i, baseSpeed*speedMultiplier[i%speedMultiplier.length], yOffset, textPadding[0]*fontSizeMultiplier[i%fontSizeMultiplier.length])
-      
-      yOffset += textPadding[1]*fontSizeMultiplier[i%fontSizeMultiplier.length] + textHeight;
+  function animate() {
+    context.resetTransform();
+    context.fillStyle = bgColor;
+    context.fillRect(0, 0, canvas.width, canvas.height);  
+    
+    context.translate(canvas.width/2, canvas.height/2);
+    context.rotate(-Math.PI/5);
+    context.translate(-canvas.width/2, -canvas.height/2); 
+
+
+    const strokeGradient = context.createRadialGradient(mousePositon[0], mousePositon[1], 0, mousePositon[0], mousePositon[1], canvas.height/2);
+    strokeGradient.addColorStop(0, txtColor);
+    strokeGradient.addColorStop(1, bgColor);
+    context.strokeStyle = strokeGradient;  
+
+    var yOffset = -canvas.height/2;
+    for (let i = 0; i < lineNumber; i++) {
+      const textHeight = fontSize*fontSizeMultiplier[i%fontSizeMultiplier.length];
+      context.font = `${textHeight}px Inter`;
+
+      drawLine(i, baseSpeed*speedMultiplier[i%speedMultiplier.length], yOffset)
+
+      yOffset += textPadding*fontSizeMultiplier[i%fontSizeMultiplier.length] + textHeight;
     }   
   }
 
 
-  function drawLine(lineIndex, speed, y, padding) {
-    const textWidth = context.measureText(text).width;
-    const blockSize = textWidth + padding;
+  function drawLine(lineIndex, speed, y) {
+    const textWidth = context.measureText(text + ' ').width;
+    const blockSize = textWidth;
     const blockNumber = Math.ceil(canvas.width / blockSize);
 
     if (xOffsets[lineIndex] <= blockSize) {
@@ -61,9 +76,14 @@ function drawMovingText(text) {
       xOffsets[lineIndex] = 0
     }
 
+    let printText = '';
     for (let i = 0; i < (blockNumber + 1); i++) {
-      context.strokeText(text, xOffsets[lineIndex] + (i-1)*blockSize, y);
+      printText += text;
+      printText += ' ';
     }
+
+    const x = xOffsets[lineIndex] - blockSize;
+    context.strokeText(printText, x, y);
   }
 
 }
@@ -77,7 +97,20 @@ function titleAnimation(delay) {
     i = i ? 0 : 1;
     title_cursor.style.opacity = i;
   }, delay);
+
+  const title = document.getElementById('title');
+  title.innerText = '';
+
+  setTimeout(() => {
+    printText(title, 'onxzy', 100)
+  }, 500);
+
+  function printText(element, remainingText, speed) {
+    if (!remainingText) return
+    setTimeout(() => {
+      element.innerText += remainingText[0];
+      printText(element, remainingText.slice(1), speed) ;
+    }, speed);
+  }
 }
-
-
 
